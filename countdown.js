ae6397fd655679a1e84a9374b1ea435dc3e8bd6e
mikththeshop <mikththeshop@gmail.com>
2023-08-22 07:06:19 +0100

    coming soon...

---
+++ countdown.js
@@ -0,0 +1,29 @@
+// Get the target date (October 1, 2023)
+const targetDate = new Date('October 1, 2023 00:00:00').getTime();
+
+const countdownInterval = setInterval(() => {
+    const now = new Date().getTime();
+    const timeRemaining = targetDate - now;
+
+    if (timeRemaining > 0) {
+        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+        updateCountdown(days, hours, minutes, seconds);
+    } else {
+        updateCountdown(0, 0, 0, 0);
+        clearInterval(countdownInterval);
+    }
+}, 1000);
+
+function updateCountdown(days, hours, minutes, seconds) {
+    document.getElementById('days').textContent = days;
+    document.getElementById('hours').textContent = hours;
+    document.getElementById('minutes').textContent = minutes;
+    document.getElementById('seconds').textContent = seconds;
+}
+window.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+});
